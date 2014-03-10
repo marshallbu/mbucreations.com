cdd4f144763e04961c7135840e48fac27f20a55e
Marshall Upshur <marshall@mbucreations.com>
2014-03-10 16:09:48 -0700

    initial commit

---
+++ index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="description" content="">
+    <meta name="author" content="">
+    <title>mbuCreations - something a little brighter, coming soon</title>
+    
+    <!-- Typekit -->
+    <script type="text/javascript" src="//use.typekit.net/wxq2eag.js"></script>
+    <script type="text/javascript">try{Typekit.load();}catch(e){}</script>
+
+    <link href="app/styles/css/main.css" rel="stylesheet">
+
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+    <![endif]-->
+  </head>
+  <body>
+    <div id="app_splash" class="">
+      <div class="splash_content">
+        <div class="logo"></div>
+        <div class="coming-soon pull-right">a brighter experience, <span class="colorize">coming soon</span></div>
+      </div>
+
+      <div class="external-forward">
+        <a href="http://www.marshallupshur.com">in the meantime, check out this <i class="fa fa-angle-double-right"></i></a>
+      </div>
+    </div>
+
+  </body>
+</html>
